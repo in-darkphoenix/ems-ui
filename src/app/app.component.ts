8f657f919339cbc2c5d653527a8cd63d44f0c6fb
Ankit Kumar <213ankit.kumar@gmail.com>
2024-06-08 14:02:21 +0530

    ui update for accounts
    add navbar sidenavbar header components modules services configs and material ui

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,23 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MaterialModule } from './material/material.module';
+import { PageHeaderComponent } from './components/layouts/page-header/page-header.component';
+import { SideNavComponent } from './components/layouts/side-nav/side-nav.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    MaterialModule,
+    PageHeaderComponent,
+    SideNavComponent,
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'ems-ui';
+
+  isMenuClicked: boolean = false;
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { PageNotFoundComponent } from './components/layouts/page-not-found/page-not-found.component';
+import { AccountsComponent } from './pages/accounts/accounts.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'account', component: AccountsComponent },
+  { path: '', redirectTo: 'account', pathMatch: 'full' },
+  { path: '**', component: PageNotFoundComponent },
+];
